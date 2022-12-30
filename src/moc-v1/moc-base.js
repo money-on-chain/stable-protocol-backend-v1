d03e43153a4048269552e3e253a612b569cab35c
mulonemartin <mulone.martin@gmail.com>
2022-12-30 14:59:41 -0300

    settlement execute

--- src/moc-v1/moc-base.js
+++ src/moc-v1/moc-base.js
@@ -1,6 +1,8 @@
 import {sendTransaction} from "../transaction.js";
 import {toContractPrecisionDecimals} from "../utils.js";
 import BigNumber from "bignumber.js";
+import {statusFromContracts} from "./contracts.js";
+import Web3 from "web3";
 
 const AllowanceUseWrapper = async (web3, dContracts, token, allow, tokenDecimals) => {
 
@@ -32,6 +34,41 @@ const AllowanceUseWrapper = async (web3, dContracts, token, allow, tokenDecimals
     return { receipt, filteredEvents }
 }
 
+const SettlementExecute = async (web3, dContracts, configProject) => {
+
+    const userAddress = `${process.env.USER_ADDRESS}`.toLowerCase()
+    const MocCABag = dContracts.contracts.MocCABag
+    const MocCABagAddress = MocCABag.options.address
+
+    // Get information from contracts
+    const dataContractStatus = await statusFromContracts(web3, dContracts, configProject)
+
+    // Get if block to settlement > 0 to continue
+    const getBts = new BigNumber(dataContractStatus.getBts)
+    if (getBts.gt(0)) throw new Error(`Not time to execute settlement`)
+
+    const valueToSend = null
+
+    // Calculate estimate gas cost
+    const estimateGas = await MocCABag.methods
+        .execSettlement()
+        .estimateGas({ from: userAddress, value: '0x' })
+
+    // encode function
+    const encodedCall = MocCABag.methods
+        .execSettlement()
+        .encodeABI()
+
+    // send transaction to the blockchain and get receipt
+    const { receipt, filteredEvents } = await sendTransaction(web3, valueToSend, estimateGas, encodedCall, MocCABagAddress)
+
+    console.log(`Transaction hash: ${receipt.transactionHash}`)
+
+    return { receipt, filteredEvents }
+}
+
+
 export {
-    AllowanceUseWrapper
+    AllowanceUseWrapper,
+    SettlementExecute
 }
